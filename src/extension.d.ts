e8d7fd791e1370221554cf8465d1089a49215d93
Tawsif Ibne Azad <tawsifibneazad@vl965-172-31-56-133.wireless.umass.edu>
2025-11-19 20:52:00 -0500

    Address review comments: cleanup, ignore build artifacts, newline fix

--- src/extension.d.ts
+++ src/extension.d.ts
@@ -2,3 +2,4 @@ import * as vscode from 'vscode';
 export declare function activate(context: vscode.ExtensionContext): void;
 export declare function deactivate(): void;
 //# sourceMappingURL=extension.d.ts.map
+
